dd96c5ce5f340a977a12ca150c2e8166d7b74726
Serg Zheleznikov <polwen@yandex.ru>
2020-07-27 13:22:51 +0300

    08 lazyload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
+
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+import "lazysizes/plugins/unveilhooks/ls.unveilhooks";
+// require('intersection-observer');
+import IntersectionObserver from 'intersection-observer';
+
+// import yall from 'yall-js';
+// document.addEventListener("DOMContentLoaded", function() {
+//   yall({
+
+//   })
+// });
+
 import Test from './js/components/Test';
 import SecondTest from './js/components/SecondTest';
+
+
+// import { lazyLoading, lazyLoading2 } from './js/components/lazyLoading';
+
+// document.addEventListener("DOMContentLoaded", lazyLoading);
 
 new Test();
 new SecondTest();
@@ -8,5 +27,55 @@ console.log('it works');
 const numbers = [2, 3, 4];
 const doubledNumbers = numbers.map(number => number * 2);
 
-console.log(doubledNumbers); 
-// console.log(q); 
+console.log(doubledNumbers);
+
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   var lazyloadImages;
+
+//   if ("IntersectionObserver" in window) {
+//     lazyloadImages = document.querySelectorAll(".lazy");
+//     var imageObserver = new IntersectionObserver(function(entries, observer) {
+//       entries.forEach(function(entry) {
+//         if (entry.isIntersecting) {
+//           var image = entry.target;
+//           image.classList.remove("lazy");
+//           imageObserver.unobserve(image);
+//         }
+//       });
+//     });
+
+//     lazyloadImages.forEach(function(image) {
+//       imageObserver.observe(image);
+//     });
+//   } else {
+//     var lazyloadThrottleTimeout;
+//     lazyloadImages = document.querySelectorAll(".lazy");
+
+//     function lazyload() {
+//       if (lazyloadThrottleTimeout) {
+//         clearTimeout(lazyloadThrottleTimeout);
+//       }
+
+//       lazyloadThrottleTimeout = setTimeout(function() {
+//         var scrollTop = window.pageYOffset;
+//         lazyloadImages.forEach(function(img) {
+//           if (img.offsetTop < (window.innerHeight + scrollTop)) {
+//             img.src = img.dataset.src;
+//             img.classList.remove('lazy');
+//           }
+//         });
+//         if (lazyloadImages.length == 0) {
+//           document.removeEventListener("scroll", lazyload);
+//           window.removeEventListener("resize", lazyload);
+//           window.removeEventListener("orientationChange", lazyload);
+//         }
+//       }, 20);
+//     }
+
+//     document.addEventListener("scroll", lazyload);
+//     window.addEventListener("resize", lazyload);
+//     window.addEventListener("orientationChange", lazyload);
+//   }
+// })
